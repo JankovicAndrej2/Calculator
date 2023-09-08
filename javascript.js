aff2ff5dcac41a67243139e09639f39765d135fd
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2023-09-08 11:20:34 +0200

    Funkcija za arrayubroj i event za add

--- javascript.js
+++ javascript.js
@@ -2,17 +2,42 @@ const swapElements = (myArray, index1, index2) => {
     [myArray[index1], myArray[index2]] = [myArray[index2], myArray[index1]];
 };
 
+function arrayToNumber(arr) {
+   let result = 0;
+
+   for(let i = 0; i<arr.length ;i++)
+   {
+    result += arr[i] * Math.pow(10, arr.length-i-1);
+   }
+   return result;
+}
+
+
+function add(a,b)
+{
+    return a+b;
+}
+
+
+//-------------------------------------
+ 
 let current_input = [0];
+let current_output = 0;
+
+
 
 let numbers = document.querySelectorAll(".num");
 let input = document.querySelector(".input");
-let output = document.querySelector(".output")
+let output = document.querySelector(".output");
 
 let divide = document.querySelector(".divide");
-let multiply = document.querySelector(".multyply");
+let multiply = document.querySelector(".multiply");
 let minus = document.querySelector(".minus");
 let plus = document.querySelector(".plus");
 let equal = document.querySelector(".equal");
+
+let math = [...document.querySelectorAll(".math")];
+
 
 
 //fixing id of numbers 
@@ -25,6 +50,8 @@ swapElements(numbers,1,4);
 swapElements(numbers,1,5);
 swapElements(numbers,1,6);
 swapElements(numbers,2,9);
+
+
 
 
 
@@ -41,17 +68,39 @@ for(let i = 0; i<numbers.length; i++)
        if(current_input === [0])
        {
             
-            current_input = i;
+        current_input = i; 
        }
        else{
            
             current_input.push(i);
-       }
+        }
 
-       console.log(i);
-       console.log(current_input);
+
+        input.innerHTML = arrayToNumber(current_input);
+        console.log(arrayToNumber(current_input));
+        console.log(output.firstChild.textContent);
     });
 }
+
+
+
+plus.addEventListener('click', () =>{
+    
+    current_output = arrayToNumber(current_input) + Number(output.firstChild.textContent);
+    output.innerHTML = current_output;
+    input.innerHTML = 0;
+    current_input = [0];
+})
+
+
+
+
+
+
+
+
+
+
 
 
 

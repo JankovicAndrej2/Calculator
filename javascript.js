79194c512dad979e69c0fb851b0afb03cb8c93ac
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2023-09-10 21:54:12 +0200

    Popravljen -, sve osnovno radi

--- javascript.js
+++ javascript.js
@@ -2,14 +2,22 @@ const swapElements = (myArray, index1, index2) => {
     [myArray[index1], myArray[index2]] = [myArray[index2], myArray[index1]];
 };
 
+
 function arrayToNumber(arr) {
    let result = 0;
+   let negat = 1;
+   let start = 0;
+   if(arr[0] == '-')
+   {
+    negat = -1;
+    start= 1;
+   }
 
-   for(let i = 0; i<arr.length ;i++)
+   for(let i = start; i<arr.length ;i++)
    {
     result += arr[i] * Math.pow(10, arr.length-i-1);
    }
-   return result;
+   return  negat * result;
 }
 
 
@@ -54,54 +62,83 @@ swapElements(numbers,2,9);
 
 let currentSymbol = "";
 let current_input = [0];
-
-for(let i = 0; i<10;i++)
-{
-    numbers[i].addEventListener('click', () =>{
-        current_input.push(i);
-        input.innerHTML = currentSymbol + " " + arrayToNumber(current_input);
-    })
-}
-
+let isEqualClicked = false;
 let count = 0;
 let isFirst = false;
 let isFirstNegative = false;
 let lastMath = "";
+
+
+for(let i = 0; i<10;i++)
+{
+    
+    numbers[i].addEventListener('click', () =>{
+        if(isEqualClicked)
+        {
+            isEqualClicked = false;
+            output.innerHTML = "0";
+        }
+        current_input.push(i);
+        if(currentSymbol == "=")
+        {
+            currentSymbol = ""; 
+        }
+        
+        input.innerHTML = currentSymbol + " " + arrayToNumber(current_input);
+    })
+}
+
+
+
 math.forEach((element) =>{
     element.addEventListener('click', ()=>{
+        
         currentSymbol = element.firstChild.nodeValue;
-        input.innerHTML = element.firstChild.nodeValue + " " +"0";
+        if(currentSymbol == "=")
+        {
+            currentSymbol = ""; 
+        }
+
+        input.innerHTML = currentSymbol + " " +"0";
         count++;
         let x; 
-        console.log(current_input);
+        
         if(count == 1)
         {
             isFirst = true;
-
-            if(element.firstChild.nodeValue == '-')
-            {
-                isFirstNegative = true;
-            }
         }
 
-        if(count == 2 && isFirstNegative)
+        if(element.firstChild.nodeValue == '-' && count == 1)
+         {
+            isFirstNegative = true;
+         }
+
+       
+        if(isFirst){
+            output.innerHTML = arrayToNumber(current_input);
+            isFirst = false;
+            isFirstNegative = false;
+            //input.innerHTML = "0";
+        }
+        else if(count == 2 && isFirstNegative)
         {
-            output.innerHTML = Math.abs(arrayToNumber(current_input));
+            output.innerHTML = -1 * arrayToNumber(current_input) ;
             //input.innerHTML = "0";
             isFirstNegative = false;
+            isFirst = false;
 
         }
-        else if(count == 1){
-            output.innerHTML = arrayToNumber(current_input);
-            //input.innerHTML = "0";
-        }
         else{
+            //if(output.firstChild.nodeValue == "")
+            //{
+             //   output.innerHTML = 0;
+            //}
             switch(lastMath){
                 case '+': 
                     output.innerHTML =arrayToNumber(output.firstChild.nodeValue) + arrayToNumber(current_input); 
                     break;
                 case '-': 
-                    output.innerHTML = arrayToNumber(output.firstChild.nodeValue) - arrayToNumber(current_input); 
+                    output.innerHTML = arrayToNumber(output.firstChild.nodeValue) - Math.abs(arrayToNumber(current_input)); 
                     break;
                 case 'x': 
                     output.innerHTML = arrayToNumber(output.firstChild.nodeValue) * arrayToNumber(current_input); 
@@ -109,17 +146,20 @@ math.forEach((element) =>{
                 case '/': 
                     output.innerHTML = arrayToNumber(output.firstChild.nodeValue) / arrayToNumber(current_input); 
                     break;
-                
-
+                default:
+                    break;
             }
         }
+
+
+        if(element.firstChild.nodeValue == '='){
+            count = 0;
+            input.innerHTML = 0;
+            isEqualClicked = true;
+        }
+        
+
        
-        
-
-        console.log(output.firstChild.nodeValue);
-        
-
-        console.log(lastMath);
 
         lastMath = element.firstChild.nodeValue;
         current_input = [0];
